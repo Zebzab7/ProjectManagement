122dfe19001dbd19d5fa1cbf90bcc25e2a2c796f
SimonJ <yconsj@gmail.com>
2020-04-16 14:17:45 +0200

    task test

---
+++ SoftwareHuset/src/SoftwareHuset/WorkerList.java
@@ -0,0 +1,15 @@
+package softwareHuset;
+
+import java.util.ArrayList;
+
+public class WorkerList {
+	private ArrayList<Worker> workers = new ArrayList<Worker>();
+	
+	public void addWorker(Worker worker) {
+		workers.add(worker);
+	}
+	public ArrayList<Worker> Workers(){
+		return workers;
+	}
+
+}
