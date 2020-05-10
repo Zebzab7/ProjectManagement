b4c35ad1d931eba1ae75e337ca4347a244362c9e
Zebzab7 <56087724+Zebzab7@users.noreply.github.com>
2020-05-10 16:45:25 +0200

    Revert to activity fix

---
+++ trash/test/test_helpers/ErrorMessageHolder.java
@@ -0,0 +1,15 @@
+package test_helpers;
+
+public class ErrorMessageHolder {
+	private String errorMessage = "";
+
+	public ErrorMessageHolder() {}
+	
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+}
